c7d04ba1245bc4b48b3341cb9042d3200c0e4a39
Micaela Lescano <lescano.micaela@gmail.com>
2018-11-24 10:57:04 +0100

    middleware route example impletmented

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ var port    =   process.env.PORT || 8080;
 // get an instance of router
 var router = express.Router();
 
+// route middleware that will happen on every request for exmaple to handle authentification before request is process
+    router.use(function(req, res, next) {
+
+    // log each request to the console
+    console.log(req.method, req.url);
+
+    // continue doing what we were doing and go to the route
+    next(); 
+});
 
 // home page route (http://localhost:8080)
 router.get('/', function(req, res) {
